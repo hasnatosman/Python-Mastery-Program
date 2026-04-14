baed2aabf33613f443ee3faae9662a5c1e0c01a9
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2026-04-14 22:40:54 +0600

    Fundamentals
    Python basic informations

---
+++ pythonfundamentals.py
@@ -0,0 +1,138 @@
+# Module 1: Introduction to Python
+# This module introduces the Python programming language and its ecosystem.
+# What is Python and why it is widely used
+# Applications of Python (data analysis, automation, web development, AI)
+# Installing Python
+# Installing VS Code / IDE setup
+# Writing the first Python program
+# Understanding Python syntax and indentation
+# Running Python scripts
+# Using Python interactive shell
+# Practice:
+# Hello World program
+# Simple arithmetic program
+
+# What is Python?
+# Python is a high-level programming language.
+
+# Simple meaning:
+
+# Easy to read
+# Easy to write
+# Looks like English
+
+# Why Python is widely used
+# Simple syntax → beginner friendly
+# Huge community
+# Works in many fields
+# Lots of libraries (ready tools)
+
+# Applications of Python
+
+# Data Analysis
+
+
+numbers = [10, 20, 30]
+average = sum(numbers) / len(numbers)
+
+print(average)
+
+# Used in:
+#
+# Data science
+# Business analysis
+
+
+# Automation
+
+for i in range(5):
+    print("Task running...")
+
+# Used for:
+
+# Repeating tasks
+# File handling
+# Web scraping
+
+
+# Web Development
+# Backend development using frameworks like Django
+
+# Example idea:
+# Login system
+# APIs
+# Dashboards
+
+
+# AI / Machine Learning
+
+
+# simple logic example
+temp = 50
+if temp > 30:
+    print("Hot day")
+
+# Real AI uses libraries like:
+
+# TensorFlow
+# PyTorch
+
+
+#  Installing Python
+
+# Steps
+
+# Go to: [https://python.org](https://python.org)
+# Download Python
+# Install with:
+# ✔ “Add Python to PATH”
+
+# Verify installation
+
+# Bash
+# python --version
+
+
+
+#  Installing VS Code (IDE)
+
+#  Steps
+
+# Download Visual Studio Code
+# Install Python extension
+# Open folder → start coding
+
+
+# First Python Program
+
+# Hello World
+
+print("Hello, World!")
+
+# Output:
+
+# Hello, World!
+
+
+# Python Syntax & Indentation
+
+# Important Rule
+
+# Python uses indentation (spaces) instead of {}
+
+# Always use 4 spaces (best practice)
+
+# Running Python Scripts
+
+# Method 1: Terminal
+
+# bash
+# python file_name.py
+
+# summary
+
+# Python = easy + powerful
+# Used in data, web, automation, AI
+# Indentation is very important
+# Run code via terminal or VS Code
+# Use shell for quick tests
